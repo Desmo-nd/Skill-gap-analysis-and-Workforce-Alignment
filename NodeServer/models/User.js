56119a7140f234d62342ef3ea75c6e829609d036
Desmo-nd <mwrgdesmond@gmail.com>
2024-03-07 06:46:36 +0300

    updated

--- NodeServer/models/User.js
+++ NodeServer/models/User.js
@@ -4,7 +4,6 @@ const UserSchema = new mongoose.Schema({
     username: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    location: {type: String, required: true},
          
 }, { timestamps: true });
 
