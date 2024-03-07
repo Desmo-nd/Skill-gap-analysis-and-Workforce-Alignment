99d7a2467a2d24fa467c26ebc59064f033d17a0f
Desmo-nd <mwrgdesmond@gmail.com>
2024-03-07 06:23:23 +0300

    working node

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -10,6 +10,7 @@ dotenv.config();
 mongoose.connect(process.env.MONGO_URL).then(() => console.log('db connected')).catch((err) => console.log(err));
 
 app.use(cors());
+app.use(express.json());
 app.use('/api/', authRouter);
 
 
